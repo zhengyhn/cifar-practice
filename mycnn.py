fc96ea14a599cdbea5233b01cef9a8fa4bf8a2c1
Hank <yuanhang.zheng@qq.com>
2021-07-13 16:01:19 +0000

    try attention

--- mycnn.py
+++ mycnn.py
@@ -2,44 +2,116 @@ import torch
 import numpy as np
 import torch.nn as nn
 from trainer import Trainer
+import torch.nn.functional as F
+
+torch.backends.cudnn.enabled = False
 
 class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
 
+class UnFlatten(nn.Module):
+    def __init__(self, H, W):
+        super(UnFlatten, self).__init__()
+        self.H = H
+        self.W = W
+
+    def forward(self, input):
+        return input.view(input.shape[0], -1, self.H, self.W)
+
+class Scale(nn.Module):
+    def __init__(self, origin):
+        super(Scale, self).__init__()
+        self.origin = origin
+
+    def forward(self, input):
+        #return input.reshape(input.shape[0], input.shape[1], 1, 1) * self.origin
+        return input * self.origin
+
+class SEBlock(nn.Module):
+    def __init__(self, ratio, device):
+        super(SEBlock, self).__init__()
+        self.ratio = ratio
+        self.device = device
+
+    def forward(self, input):
+        _, C, H, W = input.shape
+        down_size = int(C // self.ratio)
+        modules = [
+                nn.AvgPool2d(kernel_size=H),
+                nn.Conv2d(in_channels=C, out_channels=down_size, kernel_size=1),
+                #nn.BatchNorm2d(down_size),
+                nn.ReLU(True),
+                nn.Conv2d(in_channels=down_size, out_channels=C, kernel_size=1),
+                #nn.BatchNorm2d(C),
+                nn.Sigmoid(),
+                Scale(input),
+        ]
+        output = input
+        for module in modules:
+            output = module.to(self.device)(output)
+        return output
+
+class NonLocalBlock(nn.Module):
+    def __init__(self, C):
+        super(NonLocalBlock, self).__init__()
+        self.pool = nn.MaxPool2d(2, 2)
+        self.down_size = C // 2
+        self.theta = nn.Conv2d(in_channels=C, out_channels=self.down_size, kernel_size=1)
+        self.phi = nn.Conv2d(in_channels=C, out_channels=self.down_size, kernel_size=1)
+        self.g = nn.Conv2d(in_channels=C, out_channels=self.down_size, kernel_size=1)
+        self.w = nn.Conv2d(in_channels=self.down_size, out_channels=C, kernel_size=1)
+        nn.init.constant_(self.w.weight, 0)
+        nn.init.constant_(self.w.bias, 0)
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        theta_x = self.theta(x).view(N, C, -1).permute(0, 2, 1)
+        phi_x = self.pool(self.phi(x)).view(N, C, -1)
+        #phi_x = self.phi(x).view(N, C, -1)
+        y = torch.matmul(theta_x, phi_x) / np.sqrt(phi_x.shape[-1])
+        g_x = self.pool(self.g(x)).view(N, C, -1).permute(0, 2, 1)
+        #g_x = self.g(x).view(N, C, -1).permute(0, 2, 1)
+        #print(y.shape, g_x.shape)
+        z = torch.matmul(F.softmax(y, dim=-1), g_x)
+        return self.w(z.view(N, H, W, self.down_size).permute(0, 3, 1, 2)) + x
+
 class CNN(Trainer):
     def __init__(self):
         Trainer.__init__(self, train_percentage=0.95, train_batch_size=512)
         layers = [
-                ['conv', 3, 64, 3, 1], ['norm'], ['relu'],
-                ['conv', 64, 64, 3, 1], ['norm'], ['relu'],
-                ['conv', 64, 64, 3, 1], ['norm'], ['relu'],
-                #['dropout', 0.2],
-                ['maxPool', 2, 2],
-                ['conv', 64, 128, 3, 1], ['norm'], ['relu'],
-                ['conv', 128, 128, 3, 1], ['norm'], ['relu'],
-                ['conv', 128, 128, 3, 1], ['norm'], ['relu'],
-                #['dropout', 0.2],
-                ['maxPool', 2, 2],
-                ['conv', 128, 256, 3, 1], ['norm'], ['relu'],
-                ['conv', 256, 256, 3, 1], ['norm'], ['relu'],
-                ['conv', 256, 256, 3, 1], ['norm'], ['relu'],
-                #['dropout', 0.2],
-                ['maxPool', 2, 2],
-                ['conv', 256, 512, 3, 1], ['norm'], ['relu'],
-                ['conv', 512, 512, 3, 1], ['norm'], ['relu'],
-                ['conv', 512, 512, 3, 1], ['norm'], ['relu'],
-                ['avgPool', 4, 1],
-                ['conv', 512, 64, 1, 0], ['norm'], ['relu'],
-                ['dropout', 0.5],
-                ['conv', 64, 64, 1, 0], ['norm'], ['relu'],
-                ['dropout', 0.5],
+                ['conv', 3, 64, 3, 1], ['norm'], ['relu'], 
+                ['nonlocal', 64],
+                #['conv', 3, 64, 3, 1], ['norm'], ['relu'], 
+                #['conv', 64, 64, 3, 1], ['norm'], ['relu'], 
+                ##['conv', 64, 64, 3, 1], ['norm'], ['relu'],
+                ##['seblock'],
+                #['maxPool', 2, 2],
+                #['conv', 64, 128, 3, 1], ['norm'], ['relu'], 
+                #['conv', 128, 128, 3, 1], ['norm'], ['relu'], 
+                ##['conv', 128, 128, 3, 1], ['norm'], ['relu'], 
+                ##['seblock'],
+                ##['nonlocal', 128],
+                #['maxPool', 2, 2],
+                #['conv', 128, 256, 3, 1], ['norm'], ['relu'], 
+                #['conv', 256, 256, 3, 1], ['norm'], ['relu'], 
+                ##['conv', 256, 256, 3, 1], ['norm'], ['relu'], 
+                ##['seblock'],
+                ##['nonlocal', 256],
+                #['maxPool', 2, 2],
+                #['conv', 256, 512, 3, 1], ['norm'], ['relu'], 
+                #['conv', 512, 512, 3, 1], ['norm'], ['relu'], 
+                #['conv', 512, 512, 3, 1], ['norm'], ['relu'], 
+                #['seblock'],
+                #['nonlocal', 512],
+                ['avgPool', 32, 1],
+                #['conv', 512, 256, 1, 0], ['norm'], ['relu'],
+                #['dropout', 0.5],
+                #['conv', 256, 256, 1, 0], ['norm'], ['relu'],
+                #['dropout', 0.5],
                 ['flatten'],
-                #['flatten'],
-                #['fc', 1 * 1 * 512, 64], ['relu'],
-                #['dropout', 0.5],
-                #['fc', 64, 64], ['relu'],
-                #['dropout', 0.5],
+                ['fc', 64, 64], ['relu'],
+                ['dropout', 0.5],
                 ['fc', 64, self.num_label]
         ]
         activations = []
@@ -47,22 +119,27 @@ class CNN(Trainer):
             layer = layers[i]
             activation = None
             if layer[0] == 'conv':
-                activation = nn.Conv2d(in_channels=layer[1], out_channels=layer[2], kernel_size=layer[3], padding=layer[4]).to(self.device)
+                activation = nn.Conv2d(in_channels=layer[1], out_channels=layer[2], kernel_size=layer[3], padding=layer[4])
             elif layer[0] == 'norm':
-                activation = nn.BatchNorm2d(layers[i - 1][2]).to(self.device)
+                activation = nn.BatchNorm2d(layers[i - 1][2])
             elif layer[0] == 'relu':
                 activation = nn.ReLU(True)
             elif layer[0] == 'maxPool':
-                activation = nn.MaxPool2d(layer[1], layer[2]).to(self.device)
+                activation = nn.MaxPool2d(layer[1], layer[2])
             elif layer[0] == 'avgPool':
-                activation = nn.AvgPool2d(layer[1], layer[2]).to(self.device)
+                activation = nn.AvgPool2d(layer[1], layer[2])
             elif layer[0] == 'dropout':
                 activation = nn.Dropout(layer[1])
             elif layer[0] == 'flatten':
                 activation = Flatten()
             elif layer[0] == 'fc':
-                activation = nn.Linear(layer[1], layer[2]).to(self.device)
-            activations.append(activation)
+                activation = nn.Linear(layer[1], layer[2])
+            elif layer[0] == 'seblock':
+                activation = SEBlock(4, self.device)
+            elif layer[0] == 'nonlocal':
+                activation = NonLocalBlock(layer[1])
+            if activation:
+                activations.append(activation.to(self.device))
         self.module_list = nn.ModuleList(activations)
 
     def map_feature(self, inputs):
@@ -76,5 +153,5 @@ class CNN(Trainer):
 
 cnn = CNN()
 cnn.to(cnn.device)
-model = cnn.train_model(num_epoch_to_log=5, learning_rate=1e-3, weight_decay=0.001, epochs=60)
+model = cnn.train_model(warmup_epochs=5, num_epoch_to_log=1, learning_rate=1e-3, weight_decay=0.001, epochs=50)
 cnn.test(model)
--- trainer.py
+++ trainer.py
@@ -3,8 +3,29 @@ import torchvision
 import torchvision.transforms as transforms
 import numpy as np
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 import copy
+import math
+from apex import amp
+
+from torch.utils.data import DataLoader
+from prefetch_generator import BackgroundGenerator
+
+class DataLoaderX(DataLoader):
+    def __iter__(self):
+        return BackgroundGenerator(super().__iter__())
+
+class LabelSmoothCELoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, pred, label, smoothing=0.1):
+        pred = F.softmax(pred, dim=1)
+        one_hot_label = F.one_hot(label, pred.size(1)).float()
+        smoothed_one_hot_label = (1.0 - smoothing) * one_hot_label + smoothing / pred.size(1)
+        loss = (-torch.log(pred)) * smoothed_one_hot_label
+        return loss.sum(axis=1, keepdim=False).mean()
 
 class Trainer(nn.Module):
     def __init__(self, train_percentage=0.95, num_feature=3 * 32 * 32, train_batch_size=400):
@@ -15,15 +36,16 @@ class Trainer(nn.Module):
 
         train_transform = transforms.Compose([
             transforms.RandomHorizontalFlip(),
-            transforms.ColorJitter(),
-            transforms.RandomCrop(32, padding=4),
+            #transforms.ColorJitter(),
+            transforms.RandomCrop(32, padding=2),
+            #transforms.RandomRotation((5, -5)),
             transforms.ToTensor(),
-            #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             #transforms.Normalize((0.5, 0.5, 0.5), (0.2, 0.2, 0.2)),
         ])
         transform = transforms.Compose([
             transforms.ToTensor(),
-            #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             #transforms.Normalize((0.5, 0.5, 0.5), (0.2, 0.2, 0.2)),
         ])
         trainset = torchvision.datasets.CIFAR10(root='./dataset', train=True, download=False, transform=train_transform)
@@ -32,14 +54,18 @@ class Trainer(nn.Module):
         random_indexes = np.random.permutation(range(0, len(trainset.data)))
         num_train = int(len(trainset.data) * train_percentage)
         num_val = min(len(trainset.data) - num_train, int(num_train * 0.1))
+        num_train = len(trainset.data) - num_val
         train_indexes = random_indexes[:num_train]
         trainset.data = trainset.data[train_indexes]
         trainset.targets = list(np.array(trainset.targets)[train_indexes])
-        self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=train_batch_size, shuffle=True, drop_last=True, pin_memory=True)
+        #self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=train_batch_size, shuffle=True, drop_last=True, pin_memory=True)
+        self.trainloader = DataLoaderX(trainset, batch_size=train_batch_size, shuffle=True, drop_last=True, pin_memory=True, num_workers=8)
+
         validate_indexes = random_indexes[num_train:num_train + num_val]
         validateset.data = validateset.data[validate_indexes]
         validateset.targets = list(np.array(validateset.targets)[validate_indexes])
-        self.validateloader = torch.utils.data.DataLoader(validateset, batch_size=len(validateset.data), pin_memory=True)
+        #self.validateloader = torch.utils.data.DataLoader(validateset, batch_size=len(validateset.data), pin_memory=True, num_workers=4)
+        self.validateloader = DataLoaderX(validateset, batch_size=len(validateset.data), pin_memory=True, num_workers=8)
 
         # load from memory
         #trainloader = torch.utils.data.DataLoader(trainset, batch_size=len(trainset.data), shuffle=True)
@@ -48,6 +74,7 @@ class Trainer(nn.Module):
         #train_labels = train_labels.to(self.device)
         #num_train = int(len(train_x) * train_percentage)
         #num_val = min(len(train_x) - num_train, int(num_train * 0.01))
+        #num_train = len(trainset.data) - num_val
         #self.trainset = torch.utils.data.TensorDataset(train_x[:num_train], train_labels[:num_train])
         #self.trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=train_batch_size,
         #                                                shuffle=True, drop_last=True)
@@ -65,22 +92,33 @@ class Trainer(nn.Module):
         # inputs, _, _ = torch.pca_lowrank(inputs, self.num_feature)
         return inputs.to(self.device)
 
-    def train_model(self, learning_rate=1e-4, epochs=100, num_epoch_to_log=5, weight_decay=0):
+    def train_model(self, learning_rate=1e-4, epochs=100, num_epoch_to_log=5, weight_decay=0, warmup_epochs=10):
         criterion = nn.CrossEntropyLoss()
+        #criterion = LabelSmoothCELoss()
         optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate, weight_decay=weight_decay)
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=0,
-                                                               last_epoch=-1)
+        warm_up_with_cosine_lr = lambda epoch: (epoch + 1) / warmup_epochs if epoch < warmup_epochs else \
+                0.5 * (math.cos((epoch - warmup_epochs) / (epochs - warmup_epochs) * math.pi) + 1)
+        scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=warm_up_with_cosine_lr)
+        #scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=0,
+        #                                                       last_epoch=-1)
+        #model, optimizer = amp.initialize(self, optimizer, opt_level='O1')
+        model = self
+        num_gpu = torch.cuda.device_count()
+        if num_gpu > 1:
+            model = torch.nn.DataParallel(model, device_ids=range(num_gpu))
         for epoch in range(epochs):
             self.train()
             for inputs, labels in self.trainloader:
                 inputs = self.map_feature(inputs)
                 inputs, labels = Variable(inputs).to(self.device), Variable(labels).to(self.device)
-                outputs = self(inputs)
+                outputs = model(inputs)
                 loss = criterion(outputs, labels)
                 optimizer.zero_grad()
                 loss.backward()
+                #with amp.scale_loss(loss, optimizer) as scaled_loss:
+                #    scaled_loss.backward()
                 optimizer.step()
-            scheduler.step()
+            scheduler.step(epoch)
             if epoch % num_epoch_to_log == 0:
                 with torch.no_grad():
                     self.eval()
